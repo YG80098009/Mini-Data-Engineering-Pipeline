5825deac94065d590d3b9cc54e2def7a3b6408f5
Meir-Gerby <meirg2001@gmail.com>
2026-01-20 17:24:51 +0200

    add the service one class without sending to server b

--- app/server-a/service/coordinate_service.py
+++ app/server-a/service/coordinate_service.py
@@ -1,36 +1,23 @@
 import requests 
-from typing import Any, Dict
+from typing import Any
 
-url = "https://geocoding-api.open-meteo.com/v1/search" 
+
 class CoordinatesService:
     # will come from the env file
-    def location(self, location_name: str):
+    @staticmethod
+    def location(location_name: str):
         """create params based on the location name"""
         params: dict[str,Any] = {
             "name": location_name,
             "count": 1
         } 
         return params
-
-    def fetch_coordinates(self, url: str, params: dict[str,Any]) -> dict:
+    @staticmethod
+    def fetch_coordinates(params: dict[str,Any]) -> dict:
         """send request to the site and return the data"""
+        url = "https://geocoding-api.open-meteo.com/v1/search"
         response = requests.get(url, params=params)
         response.raise_for_status()
         data: dict = response.json()
         return data
 
-    def filter_data(self, data):
-        result = data["results"][0]
-        return {
-            "location_name": result["name"],
-            "country": result.get("country"),
-            "latitude": result["latitude"],
-            "longitude": result["longitude"]
-        }
-    
-    def check_data(self, data):
-        if "results" not in data or not data["results"]:
-            return False 
-        
-    def raise_error(self, location_name):
-        raise ValueError(f"Location not found: {location_name}")

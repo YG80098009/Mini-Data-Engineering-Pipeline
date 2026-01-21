f0cd8f495c8994ce2e4b263f8d12a000690eb632
Meir-Gerby <meirg2001@gmail.com>
2026-01-21 10:43:46 +0200

    solve conflicts

---
+++ test/server-b/routes.py
@@ -0,0 +1,12 @@
+from flask import request, jsonify
+import requests
+from service import clean_and_transform
+from main import app  
+
+@app.post('/clean')
+def clean_endpoint():
+    Final_data = clean_and_transform(request.json)
+
+    requests.post(json=Final_data)
+
+    return jsonify({"status": "ok"})

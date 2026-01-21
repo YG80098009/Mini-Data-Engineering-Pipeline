dc1a81977955bbffc8020fb9aeb27492e2d0005e
Meir-Gerby <meirg2001@gmail.com>
2026-01-21 11:17:50 +0200

    docs: add router for getting the data

--- app/server-b/routes.py
+++ app/server-b/routes.py
@@ -1,1 +1,26 @@
 
+from fastapi import APIRouter
+
+from service import clean_and_transform
+
+router = APIRouter()
+
+@router.post('/clean')
+def clean_endpoint():
+    Final_data = clean_and_transform()
+
+    requests.post(json=Final_data)
+
+    return jsonify({"status": "ok"})
+
+
+
+router = APIRouter()
+
+
+@router.get("/")
+async def root():
+    return {"hello": "world"}
+
+
+app.include_router(router)
